ab4d8c6a3eb160b9e423b68c3226931870a0131b
NyctoWalker <115701336+NyctoWalker@users.noreply.github.com>
2024-09-05 19:34:07 +0700

    lab8 simple graphics redactor done

---
+++ CG_lab8/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace CG_lab8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonGrayscale = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonTrueGrayscale = new System.Windows.Forms.Button();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonBlue = new System.Windows.Forms.Button();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(784, 461);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonSave.Location = new System.Drawing.Point(3, 38);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonOpen.Location = new System.Drawing.Point(3, 5);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonGrayscale
+            // 
+            this.buttonGrayscale.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonGrayscale.Location = new System.Drawing.Point(103, 5);
+            this.buttonGrayscale.Name = "buttonGrayscale";
+            this.buttonGrayscale.Size = new System.Drawing.Size(75, 23);
+            this.buttonGrayscale.TabIndex = 3;
+            this.buttonGrayscale.Text = "Ср. Ч/Б";
+            this.buttonGrayscale.UseVisualStyleBackColor = true;
+            this.buttonGrayscale.Click += new System.EventHandler(this.buttonGrayscale_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonOpen, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonGrayscale, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSave, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonTrueGrayscale, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRed, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonBlue, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonGreen, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 361);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonCancel.Location = new System.Drawing.Point(3, 71);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Отменить";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonTrueGrayscale
+            // 
+            this.buttonTrueGrayscale.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonTrueGrayscale.Location = new System.Drawing.Point(103, 38);
+            this.buttonTrueGrayscale.Name = "buttonTrueGrayscale";
+            this.buttonTrueGrayscale.Size = new System.Drawing.Size(75, 23);
+            this.buttonTrueGrayscale.TabIndex = 5;
+            this.buttonTrueGrayscale.Text = "Коэф. Ч/Б";
+            this.buttonTrueGrayscale.UseVisualStyleBackColor = true;
+            this.buttonTrueGrayscale.Click += new System.EventHandler(this.buttonTrueGrayscale_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonRed.Location = new System.Drawing.Point(203, 5);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(75, 23);
+            this.buttonRed.TabIndex = 6;
+            this.buttonRed.Text = "Красный";
+            this.buttonRed.UseVisualStyleBackColor = true;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
+            // 
+            // buttonBlue
+            // 
+            this.buttonBlue.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonBlue.Location = new System.Drawing.Point(203, 71);
+            this.buttonBlue.Name = "buttonBlue";
+            this.buttonBlue.Size = new System.Drawing.Size(75, 23);
+            this.buttonBlue.TabIndex = 7;
+            this.buttonBlue.Text = "Синий";
+            this.buttonBlue.UseVisualStyleBackColor = true;
+            this.buttonBlue.Click += new System.EventHandler(this.buttonBlue_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonGreen.Location = new System.Drawing.Point(203, 38);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(75, 23);
+            this.buttonGreen.TabIndex = 8;
+            this.buttonGreen.Text = "Зелёный";
+            this.buttonGreen.UseVisualStyleBackColor = true;
+            this.buttonGreen.Click += new System.EventHandler(this.buttonGreen_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pictureBox);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonGrayscale;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonTrueGrayscale;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonBlue;
+        private System.Windows.Forms.Button buttonGreen;
+    }
+}
---
+++ CG_lab8/MainForm.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_lab8
+{
+    public partial class MainForm : Form
+    {
+        private Point PreviousPoint, point;
+
+        private Bitmap bmp;
+        private Pen blackPen;
+        private Pen orangePen;
+        private Graphics g;
+        private Image initialImage;
+
+        private List<Point> polygonPoints;
+
+        public MainForm()
+        {
+            blackPen = new Pen(Color.Black, 4);
+            orangePen = new Pen(Color.Orange, 2);
+
+            InitializeComponent();
+
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = bmp;
+            g = Graphics.FromImage(pictureBox.Image);
+            g.Clear(Color.White);
+
+            polygonPoints = new();
+
+            UpdateSize(pictureBox.Width, pictureBox.Height);
+        }
+
+        private void UpdateSize(int w, int h)
+        {
+            int minWidth = MinimumSize.Width;
+            int minHeight = MinimumSize.Height - 100;
+
+            int maxWidth = Screen.PrimaryScreen.Bounds.Width;
+            int maxHeight = Screen.PrimaryScreen.Bounds.Height - 100;
+
+            int _width = w < minWidth ? minWidth : w >= maxWidth ? maxWidth : w;
+            int _height = h < minHeight ? minHeight : h >= maxHeight ? maxHeight : h;
+
+            this.Size = new Size(_width, _height + 100);
+        }
+
+
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedialog = new SaveFileDialog();
+
+            //Свойства для savedialog
+            savedialog.Title = "Сохранить картинку как ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter =
+            "Bitmap File(*.bmp)|*.bmp|" +
+            "JPEG File(*.jpg)|*.jpg|" +
+            "PNG File(*.png)|*.png";
+            savedialog.ShowHelp = true;
+
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savedialog.FileName; // Полный путь к файлу
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3); // Убираем расширение
+
+                // Сохраняем
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "png":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName.ToString() + "\\Images";
+            dialog.InitialDirectory = Directory.Exists(path) ? path : @"C:\";
+
+            // Задаем расширения файлов
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.PNG)| *.bmp; *.jpg; *.png; ";
+            if (dialog.ShowDialog() == DialogResult.OK)//вызываем диалоговое окно
+            {
+                UpdateImage(Image.FromFile(dialog.FileName));
+                polygonPoints.Clear();
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (initialImage is not null)
+                UpdateImage(initialImage);
+
+            polygonPoints.Clear();
+        }
+
+        private void UpdateImage(Image image)
+        {
+            initialImage = image; //Загружаем изображение
+
+            int width = initialImage.Width;
+            int height = initialImage.Height;
+            pictureBox.Width = width;
+            pictureBox.Height = height;
+
+            bmp = new Bitmap(initialImage, width, height);
+            pictureBox.Image = bmp; //записываем изображение в формате bmp
+            g = Graphics.FromImage(pictureBox.Image);
+
+            UpdateSize(width, height);
+        }
+
+
+
+        private void buttonGrayscale_Click(object sender, EventArgs e)
+        {
+            // Цикл по всему изображению
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+                    int G = bmp.GetPixel(i, j).G;
+                    int B = bmp.GetPixel(i, j).B;
+                    Color p = MedianGrayscale(R, G, B); // Прозрачность, R, G, B
+                    bmp.SetPixel(i, j, p); //записываем полученный цвет в точку
+                }
+
+            Refresh();
+        }
+
+        private void buttonTrueGrayscale_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+                    int G = bmp.GetPixel(i, j).G;
+                    int B = bmp.GetPixel(i, j).B;
+                    Color p = ConstGrayscale(R, G, B); // Прозрачность, R, G, B
+                    bmp.SetPixel(i, j, p); //записываем полученный цвет в точку
+                }
+
+            Refresh();
+        }
+
+        private Color MedianGrayscale(int R, int G, int B)
+        {
+            int Gray = (int)((R + G + B) / 3);
+            return Color.FromArgb(Gray, Gray, Gray);
+        }
+
+        private Color ConstGrayscale(int R, int G, int B)
+        {
+            int Gray = (int)((R * 0.2126d + G * 0.7152d + B * 0.0722d));
+            return Color.FromArgb(Gray, Gray, Gray);
+        }
+
+
+
+        private void IsolateChannel(string chanel)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    if (!IsInPolygon(new Point(x, y), polygonPoints))
+                    {
+                        Color originalColor = bmp.GetPixel(x, y);
+
+                        switch (chanel)
+                        {
+                            case "red":
+                                bmp.SetPixel(x, y, Color.FromArgb(originalColor.R, 0, 0));
+                                break;
+                            case "green":
+                                bmp.SetPixel(x, y, Color.FromArgb(0, originalColor.G, 0));
+                                break;
+                            case "blue":
+                                bmp.SetPixel(x, y, Color.FromArgb(0, 0, originalColor.B));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        int R = bmp.GetPixel(x, y).R;
+                        int G = bmp.GetPixel(x, y).G;
+                        int B = bmp.GetPixel(x, y).B;
+                        Color p = ConstGrayscale(R, G, B);
+                        bmp.SetPixel(x, y, p);
+                    }
+                }
+            }
+
+            Refresh();
+        }
+
+        private void buttonRed_Click(object sender, EventArgs e)
+        {
+            IsolateChannel("red");
+        }
+
+        private void buttonGreen_Click(object sender, EventArgs e)
+        {
+            IsolateChannel("green");
+        }
+
+        private void buttonBlue_Click(object sender, EventArgs e)
+        {
+            IsolateChannel("blue");
+        }
+
+        public static bool IsInPolygon(Point testPoint, List<Point> polygon)
+        {
+            int intersectionCount = 0;
+            int vertexCount = polygon.Count;
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                Point v1 = polygon[i];
+                Point v2 = polygon[(i + 1) % vertexCount];
+
+                // Проверка горизонтального пересечения для testPoint.Y
+                if ((v1.Y > testPoint.Y) != (v2.Y > testPoint.Y))
+                {
+                    double intersectX = (v2.X - v1.X) * (testPoint.Y - v1.Y) / (v2.Y - v1.Y) + v1.X;
+                    if (testPoint.X < intersectX)
+                        intersectionCount++;
+                }
+            }
+            // true если чётное количество пересечений, иначе false
+            return (intersectionCount % 2) == 1;
+        }
+
+
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PreviousPoint.X = e.X;
+                PreviousPoint.Y = e.Y;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                polygonPoints.Add(e.Location);
+
+                // Для наглядности, можно закомментировать
+                g.DrawEllipse(orangePen, new Rectangle(e.Location, new Size(2, 2)));
+
+                UpdateImage(pictureBox.Image);
+            }
+
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            { 
+                //запоминаем в point текущее положение курсора мыши
+                point.X = e.X;
+                point.Y = e.Y;
+                
+                //соединяем линией предыдущую точку с текущей
+                g.DrawLine(blackPen, PreviousPoint, point);
+                
+                //текущее положение курсора мыши сохраняем в PreviousPoint
+                PreviousPoint.X = point.X;
+                PreviousPoint.Y = point.Y;
+                pictureBox.Invalidate(); //Принудительно вызываем перерисовку
+            }
+        }
+    }
+}
